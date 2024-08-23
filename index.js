2839b53e69620d35184f57870fe88a33cc491249
Funnyohs <bbbbah89@gmail.com>
2024-08-23 03:50:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 //js code 
+fetch("https://pokeapi.co/api/v2/pokemon/charizard")
+      .then(res => res.json)
+      .then(data => console.log(data.sprites))
+      .catch(err => console.log('Error'))
