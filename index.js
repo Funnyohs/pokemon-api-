1838470d2a1636319eae49dcd2037bfb10f79fb5
Funnyohs <bbbbah89@gmail.com>
2024-08-27 04:25:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function T() {
      let enterValue = document.getElementById("Enter").value.toLowerCase();
      const img = document.getElementById("Img")
      let des = document.getElementById("description")
+     let baseStats = [0,1,2,3,4,5]
+     function blank (Element) {
+         return document.getElementById(Element) 
+     }
      fetch(`https://pokeapi.co/api/v2/pokemon/${enterValue}`)
       .then(res => {
            if (!res.ok){
@@ -12,7 +16,9 @@ function T() {
            } 
            return res.json()
       })
- .then(data => { img.src = data.sprites.front_default; let cry = new Audio(data.cries.latest); cry.play(); })
+ .then(data => { img.src = data.sprites.front_default; let cry = new Audio(data.cries.latest); cry.play(); baseStats.forEach((item) => {
+      return blank(data.stats.[item].stat.name).value = data.stats.[item].base_stat
+ })  })
       .catch(err => console.log('Error'))
 fetch(`https://pokeapi.co/api/v2/pokemon-species/${enterValue}`)
  .then(res => {
