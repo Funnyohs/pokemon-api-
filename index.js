ede9fb05f8d7ffb39ca8b652eee5ffe74ccaa2b8
Funnyohs <bbbbah89@gmail.com>
2025-01-02 22:29:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -23,12 +23,14 @@ function T() {
       })
  //use to get audio, sprites and stats of the pokemon requested
   .then(data => { 
-   /*
    normal = data.sprites.front_default;
    shiny = data.sprites.front_shiny;
-   */
    let cry = new Audio(data.cries.latest);
-   blank("Img").src = data.sprites.front_default;
+   if (blank("shiny").checked) {
+   blank("Img").src = shiny;
+   } else {
+   blank("Img").src = normal;
+   };
    cry.play();baseStats.forEach((item) => {
       return blank(data.stats[item].stat.name).value = data.stats[item].base_stat; totalMath += data.stats[item].base_stat
  })  })
