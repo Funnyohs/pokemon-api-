ad113d5c0e7e17e1e3738c14552cae8d27e6487c
Funnyohs <bbbbah89@gmail.com>
2025-01-02 22:57:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ if (blank("shiny").checked) {
  //use to get audio, sprites and stats of the pokemon requested
   .then(data => {
           blank("Img").src =  data.sprites.front_default;   
-  }
-})
+  })
+}
 function T() {   
      let enterValue = document.getElementById("Enter").value.toLowerCase();
      const baseStats = [0,1,2,3,4,5]
